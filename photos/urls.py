137f7d64709ee1857feced4a325841b30b3af34f
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-05-29 11:46:02 +0300

    ft:urls pattern for add

--- photos/urls.py
+++ photos/urls.py
@@ -6,5 +6,5 @@ from .import views
 urlpartterns = [
     path('', views.gallery, name='gallery'),
     path('photo/<str:pk>/', views.viewPhoto, name='photo'),
-    path('', views.gallery, name='gallery')
+    path('add/', views.addPhoto, name='add')
 ]
