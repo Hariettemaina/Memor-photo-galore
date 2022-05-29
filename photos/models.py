cdcad6265db95f6244c10b786bc73def04d92426
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-05-29 21:43:48 +0300

    ft:create catogory

--- photos/models.py
+++ photos/models.py
@@ -2,4 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length=30)
 
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name
